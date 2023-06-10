edaa996209989992a8431aba9846c8d00009a982
Darkere <Dark-277@web.de>
2023-06-10 14:08:36 +0200

    update script to use parameters and change to e9 by default

--- Run UpdateInstall.bat
+++ Run UpdateInstall.bat
@@ -1,2 +1,2 @@
-Powershell -ep RemoteSigned -file "%~dp0\updateinstall.ps1"
+Powershell -ep RemoteSigned -file "%~dp0\updateinstall.ps1" https://github.com/EnigmaticaModpacks/Enigmatica9 master
 pause
